5a898194217d5cbd2851256a5297ce1924d8aca8
Saverio Cavalcante <saveriocavalcante@gmail.com>
2023-07-02 00:00:08 +0200

    Modificato carosello

--- assets/js/carosello.js
+++ assets/js/carosello.js
@@ -1,11 +1,15 @@
 window.addEventListener('DOMContentLoaded', function() {
   var images = document.getElementsByClassName('img');
   var currentImgIndex = 0;
+  
+  // Aggiungi la classe 'active' all'immagine iniziale
+  images[currentImgIndex].classList.add('active');
 
   setInterval(function() {
+    // Rimuovi la classe 'active' dall'immagine corrente
     images[currentImgIndex].classList.remove('active');
     currentImgIndex = (currentImgIndex + 1) % images.length;
+    // Aggiungi la classe 'active' all'immagine successiva
     images[currentImgIndex].classList.add('active');
   }, 3000);
 });
-
