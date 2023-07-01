1fece05d6803775ecc04f7775ca2a26dfe2e95d5
Saverio Cavalcante <saveriocavalcante@gmail.com>
2023-07-01 23:51:07 +0200

    Avanzamento generale

--- assets/js/carosello.js
+++ assets/js/carosello.js
@@ -6,6 +6,6 @@ window.addEventListener('DOMContentLoaded', function() {
     images[currentImgIndex].classList.remove('active');
     currentImgIndex = (currentImgIndex + 1) % images.length;
     images[currentImgIndex].classList.add('active');
-  }, 1000);
+  }, 3000);
 });
 
